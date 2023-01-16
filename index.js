02860413df623a4b4b4a905693003697479a5144
Serhii Duniashenko <Flashbuster666@gmail.com>
2023-01-16 22:54:52 +0200

    First Letter Search script created

--- index.js
+++ index.js
@@ -18,3 +18,17 @@ if (firstArray.toString() === secondArray.toString()) {
 } else {
     check = false;
 };
+
+// 3. Letter finder
+
+const names = ["Andrew", "Jane", "Alan", "Kate", "Serhii"];
+let keyLetter = prompt("Enter the letter");
+const namesFound = names.filter(element => element[0] === keyLetter);
+
+// Names found array type
+console.log(namesFound); 
+ 
+// Names found as separate console logs 
+for (let index = 0; index < namesFound.length; index++) {
+    console.log(namesFound[index]); 
+}
